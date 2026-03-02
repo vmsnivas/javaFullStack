c88fc8edcec24b7cd1c1edbc8c8b93ab7337f23f
Arjun Shrinivas <vmsnivas@gmail.com>
2026-03-02 17:54:36 +0530

    CoreJava: Added code for C26 class

---
+++ backend/coreJava/C26/Largest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Largest{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three numbers");
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        
+        if(a>b && a>c) System.out.println(a + " is the largest number");
+        else if(b>a && b>c) System.out.println(b + " is the largest number");
+        else System.out.println(c + " is the largest number");
+    }
+}
---
+++ backend/coreJava/C26/LeapYear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class LeapYear{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the year : ");
+        int year = scanner.nextInt();
+
+        if(year % 100 == 0){
+            if(year % 400 == 0) System.out.println(year + " is a leap year");
+            else System.out.println(year + " is not a leap year");
+        }
+        else{
+            if(year % 4 == 0) System.out.println(year + " is a leap year");
+            else System.out.println(year + " is not a leap year");
+        }
+    }
+}
