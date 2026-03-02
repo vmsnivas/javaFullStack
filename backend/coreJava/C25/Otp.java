d4962a4954bd193153a5cfbee63b98a15f9413d1
Arjun Shrinivas <vmsnivas@gmail.com>
2026-03-02 17:09:47 +0530

    CoreJava: Added code for C25 class

---
+++ backend/coreJava/C25/Otp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+class Otp{
+    public static void main(String[] args){
+        Random random = new Random();
+
+        int otp = 100000 + random.nextInt(899999);
+
+        System.out.println("OTP for account verification is : " + otp);
+    }
+}
