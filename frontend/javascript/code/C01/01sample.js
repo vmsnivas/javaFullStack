98532998cd40b9e6d50b67661fefe12ad718d965
Arjun Shrinivas <vmsnivas@gmail.com>
2026-03-10 17:31:26 +0530

    JS: Added Class 1 code

---
+++ frontend/javascript/code/C01/01sample.js
@@ -0,0 +1,10 @@
+var employeeName = 'Anuket';
+var salary = 30000;
+var designation = 'Developer';
+
+console.log(typeof employeeName);
+console.log('Employee Name: ' + employeeName);
+console.log(typeof salary);
+console.log('Employee Salary: ' + salary);
+console.log(typeof designation  );
+console.log('Employee Designation: ' + designation  );
