1cf095803b429e89c130b5c04cbb763d87dcd009
Arjun Shrinivas <vmsnivas@gmail.com>
2026-03-10 22:48:05 +0530

    JS: Added code for Class 02 - Objects and DataTypes

---
+++ frontend/javascript/code/C02/01object.js
@@ -0,0 +1,12 @@
+var employee = {
+    name: 'John Doe',
+    salary: 30000,
+    designation: 'React Developer',
+    onsite: true,
+    hike: undefined
+};
+
+console.log(typeof employee);
+console.log(employee);
+console.log('Employee Name: ' + employee.name);
+console.log('Employee Salary: ' + employee['salary']);
---
+++ frontend/javascript/code/C02/02array.js
@@ -0,0 +1,9 @@
+var employees = ["John", "Jane", "Doe", "Smith", "Emily"];
+
+console.log(employees);
+console.log('First Employee: ' + employees[0]);
+console.log('Second Employee: ' + employees[1]);
+console.log('Third Employee: ' + employees[2]);
+console.log('Fourth Employee: ' + employees[3]);
+console.log('Fifth Employee: ' + employees[4]);
+console.log('Total Employees: ' + employees.length);
