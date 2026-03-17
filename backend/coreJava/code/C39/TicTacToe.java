b277e6a2df4c9a8277bc27bec630488a3b1e0e70
Arjun Shrinivas <vmsnivas@gmail.com>
2026-03-17 23:41:09 +0530

    CoreJava: Added C39 - TicTacToe game

--- backend/coreJava/code/C39/TicTacToe.java
+++ backend/coreJava/code/C39/TicTacToe.java
@@ -1,12 +1,112 @@
+import java.util.Scanner;
+
 class TicTacToe {
+
+    int clicks = 0;
+    boolean isGameActive = true;
+    String []cells = {"","","","","","","","",""};
+    String player = "O";
+
     void printBoard(){
+
         System.out.println("***************************");
         System.out.println("\tTic Tac Toe");
         System.out.println("***************************");
+
+        for(int i = 0; i < 9; i++){
+            if(cells[i].equals("")){
+                System.out.print(("__") + "\t");
+            }
+            else{
+                System.out.print(cells[i] + "\t");
+            }
+            if(i == 2 || i == 5) System.out.println("\n");
+        }
+
+        System.out.println();
+
     }
 
+    void checkWinner(String current){
+        if(cells[0].equals(current) && cells[1].equals(current) && cells[2].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[3].equals(current) && cells[4].equals(current) && cells[5].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[6].equals(current) && cells[7].equals(current) && cells[8].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[0].equals(current) && cells[3].equals(current) && cells[6].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[1].equals(current) && cells[4].equals(current) && cells[7].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[2].equals(current) && cells[5].equals(current) && cells[8].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[0].equals(current) && cells[4].equals(current) && cells[8].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+        else if(cells[2].equals(current) && cells[4].equals(current) && cells[6].equals(current)){
+            printBoard();
+            System.out.println(current + " won the game.");
+            System.out.println("Please restart the game!");
+            isGameActive = false;
+        }
+    }
     public static void main(String[] args) {
+
         TicTacToe ttt = new TicTacToe();
-        ttt.printBoard();
+        Scanner scanner = new Scanner(System.in);
+
+        while(ttt.isGameActive) {
+            ttt.printBoard();
+
+            if(ttt.clicks == 9){
+                System.out.println("Game Drawn.\nPlease restart the game.");
+                break;
+            }
+
+            System.out.println("\nPlayer " + ttt.player + " enter your cell number.");
+            int num = scanner.nextInt();
+
+            if((num >= 1 && num <= 9) && (ttt.cells[num - 1].equals(""))){
+                ttt.cells[num - 1] = ttt.player;
+                ttt.clicks++;
+                ttt.checkWinner(ttt.player);
+
+                if(ttt.player == "O") ttt.player = "X";
+                else ttt.player = "O";
+
+            }
+            else {
+
+                System.out.println("Wrong choice!!!");
+
+            }
+        }
     }
 }
